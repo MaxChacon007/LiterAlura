c165ee0ab33b6464ab18525033bfa503ff31085e
MaxChacon007 <157187548+MaxChacon007@users.noreply.github.com>
2024-06-10 23:12:03 -0300

    Update Principal.java

--- literalura/src/main/java/com/alura/literalura/principal/Principal.java
+++ literalura/src/main/java/com/alura/literalura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.alura.literalura.principal;
 
 import com.alura.literalura.model.Datos;
+import com.alura.literalura.model.DatosAutor;
 import com.alura.literalura.model.DatosLibros;
 import com.alura.literalura.service.ConsumoAPI;
 import com.alura.literalura.service.ConvierteDatos;
@@ -16,7 +17,8 @@ public class Principal {
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
     private Scanner teclado = new Scanner(System.in);
-    public void muestraElMenu(){
+
+    public void muestraElMenu() {
         var json = consumoAPI.obtenerDatos(URL_BASE);
         System.out.println(json);
         var datos = conversor.obtenerDatos(json, Datos.class);
@@ -30,25 +32,56 @@ public class Principal {
 //                .map(l -> l.titulo().toUpperCase())
 //                .forEach(System.out::println);
 
+        var opcion = -1;
+        while (opcion != 0) {
+            String menu = """
+                    *********************************************
+                                     BIENVENIDO
+                     Eliga una opción, por favor
+                     
+                         1 Buscar libro por titulo
+                         2 listar libros registrados
+                         3 listar autores registrados
+                         4 listar autores vivos en un determinado año
+                         5 listar libros por idioma
+                         0 - salir
+                         
+                    *********************************************
+                     """;
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            switch (opcion) {
+                case 1:
+                    libroPorTitulo();
+                    break;
+                case 2:
+                    //listarLibrosRegistrados();
+                    break;
+                case 3:
+                    //listarAutoresRegistrados();
+                    break;
+                case 4:
+                    //AutoresVivosEnAno();
+                    break;
+                case 5:
+                    //librosPorIdioma();
+                    break;
+                case 0:
+                    //System.out.println("Cerrando la aplicación...");
+                    break;
+                default:
+                    //System.out.println("Opción inválida");
+            }
 
-        String menu = """
-               *********************************************
-                                BIENVENIDO
-                Eliga una opción, por favor
-                
-                    1 Buscar libro por titulo
-                    2 listar libros registrados
-                    3 listar autores registrados
-                    4 listar autores vivos en un determinado año
-                    5 listar libros por idioma
-                    0 - salir
-                    
-               *********************************************
-                """;
-        System.out.println(menu);
 
-        //Busqueda de libros por nombre
-        
+
+        }
+    }
+
+    //Buscar libro por titulo
+    private void libroPorTitulo() {
+        System.out.println("Ingrese el nombre del libro que desea buscar");
         var tituloLibro = teclado.nextLine();
         json = consumoAPI.obtenerDatos(URL_BASE+"?search=" + tituloLibro.replace(" ","+"));
         var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
@@ -61,15 +94,18 @@ public class Principal {
         }else {
             System.out.println("Libro no encontrado");
         }
+        
+        
+        
 
-        //Trabajando con estadisticas
-        DoubleSummaryStatistics est = datos.resultados().stream()
-                .filter(d -> d.numeroDeDescargas() >0 )
-                .collect(Collectors.summarizingDouble(DatosLibros::numeroDeDescargas));
-        System.out.println("Cantidad media de descargas: " + est.getAverage());
-        System.out.println("Cantidad máxima de descargas: "+ est.getMax());
-        System.out.println("Cantidad mínima de descargas: " + est.getMin());
-        System.out.println(" Cantidad de registros evaluados para calcular las estadisticas: " + est.getCount());
-
+//        ejemplo private void buscarSerieWeb() {
+//            DatosSerie datos = getDatosSerie();
+//            Serie serie = new Serie(datos);
+//            repositorio.save(serie);
+//            //datosSeries.add(datos);
+//            System.out.println(datos);            
+                
+                
+                
     }
 }
