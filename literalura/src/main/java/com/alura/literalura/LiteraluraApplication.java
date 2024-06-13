603bd4455206fbc65fc5a5af74d3eea5063ffcc4
MaxChacon007 <157187548+MaxChacon007@users.noreply.github.com>
2024-06-12 21:55:08 -0300

    UP

--- literalura/src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,8 +1,6 @@
 package com.alura.literalura;
 
-import com.alura.literalura.model.DatosLibros;
-import com.alura.literalura.service.ConsumoAPI;
-import com.alura.literalura.service.ConvierteDatos;
+import com.alura.literalura.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,16 +13,8 @@ public class LiteraluraApplication  implements  CommandLineRunner{
 	}
 
 	@Override
-	public void run(String... args) throws Exception {//esto sale de la Clase consumo API,
-	var consumoApi = new ConsumoAPI();
-////	var	json = consumoApi.obtenerDatos("https://gutendex.com/books/");
-////		System.out.println(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-//		 var datos= conversor.obtenerDatos(json, DatosLibros.class);
-//		//System.out.println(datos);
-
-		var	json = consumoApi.obtenerDatos("https://gutendex.com/books/");
-		DatosLibros datosLibros = conversor.obtenerDatos(json,DatosLibros.class);
-		System.out.println(datosLibros);
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.muestraElMenu();
 	}
 }
--- literalura/src/main/java/com/alura/literalura/principal/Principal.java
+++ literalura/src/main/java/com/alura/literalura/principal/Principal.java
@@ -1,17 +1,49 @@
 package com.alura.literalura.principal;
 
+import com.alura.literalura.model.Datos;
+import com.alura.literalura.model.DatosLibros;
 import com.alura.literalura.service.ConsumoAPI;
 import com.alura.literalura.service.ConvierteDatos;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Principal {
 
     private static final String URL_BASE ="https://gutendex.com/books/";
     private ConsumoAPI consumoAPI = new ConsumoAPI();
-        private ConvierteDatos conversor = new ConvierteDatos();
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    private Scanner teclado =new Scanner(System.in);
 
         public void muestraElMenu(){
             var json = consumoAPI.obtenerDatos(URL_BASE);
             System.out.println(json);
+            var datos = conversor.obtenerDatos(json, Datos.class);
+            //System.out.println(datos);
 
-    }
+    //Busqueda de libros por nombre - OK
+
+            System.out.println("Ingrese el nombre del libro que desea buscar");
+            var tituloLibro = teclado.nextLine();
+            json = consumoAPI.obtenerDatos(URL_BASE+"?search=" + tituloLibro.replace(" ","+"));
+            var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+            Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
+                    .filter(l -> l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                    .findFirst();
+            if(libroBuscado.isPresent()){
+                System.out.println("Libro Encontrado ");
+                System.out.println("Titulo: "+libroBuscado.get().titulo());
+                System.out.println("Autor: "+libroBuscado.get().autor().get(0).nombre());
+                System.out.println("Idioma: "+libroBuscado.get().idiomas());
+                System.out.println("Numero de descargas: "+libroBuscado.get().numeroDeDescargas());
+            }else {
+                System.out.println("Libro no encontrado");
+            }
+
+
+        }
+
+
+
+
 }
